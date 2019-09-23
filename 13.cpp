0bfe48988587d4ca6d118c724880275d79b99df3
ahadru <ahadsheikh1999@gmail.com>
2019-09-24 00:52:00 +0600

    13 done

--- 13.cpp
+++ 13.cpp
@@ -10,14 +10,14 @@ int isOperator(char ch){
     }
 }
 
-int isIdentifier(char ch){
-    if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z'){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
+// int isIdentifier(char ch){
+//     if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z'){
+//         return 1;
+//     }
+//     else{
+//         return 0;
+//     }
+// }
 
 
 int main(){
@@ -30,8 +30,9 @@ int main(){
     for(int i = 0; i < size__; i++){
         if(expression[i] == '('){
             bracketCheck++;
-            if(i+1 < size__ && !isIdentifier(expression[i+1]) || expression[i+1] == ')'){
+            if(i+1 < size__ && !isalnum(expression[i+1]) || expression[i+1] == ')'){
                 printf("Invalid\n");
+                //cout<<"DDDDDDDDDDDDd1\n";
                 return 0;
             }
         }
@@ -39,34 +40,34 @@ int main(){
             bracketCheck--;
             if(i+1 < size__ && !isOperator(expression[i+1])){
                 printf("Invalid\n");
+                //cout<<"DDDDDDDDDDDDd2\n";
                 return 0;
             }
         }
         else if(isOperator(expression[i])){
-            if(i+1 < size__ && (!isIdentifier(expression[i+1]) && expression[i+1] != '(' || expression[i+1] == ')')){
+            if(i+1 < size__ && !isalnum(expression[i+1]) && expression[i+1] != '('){
                 printf("Invalid\n");
+                //cout<<"DDDDDDDDDDDDd3\n";
                 return 0;
             }
         }
-        else if(isIdentifier(expression[i])){
-            while(isIdentifier(expression[i])){
+        else if(isalnum(expression[i])){
+            while(isalnum(expression[i])){
                 i++;
             }
             i--;
             if(i+1 < size__ && (!isOperator(expression[i+1]) && expression[i+1] != ')')){
                 printf("Invalid\n");
+                //cout<<"DDDDDDDDDDDDd4\n";
                 return 0;
             }
-        }
-        else if(expression[i] >= '0' && expression[i] <= '9'){
-
-        }
-        else{
-
         }
     }
     if(bracketCheck == 0){
         printf("Valid\n");
+    }
+    else{
+        printf("Invalid\n");
     }
     return 0;
 }
